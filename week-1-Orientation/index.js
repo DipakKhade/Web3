15828f6cffdfeb50dabeb6c41ac6ac66894b9ae6
= <=>
2024-08-03 12:23:51 +0530

    hashing in blockchains

---
+++ week-1-Orientation/index.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+
+const name = '100xdevs';
+
+let hash = crypto.createHash('sha256').update(name).digest('hex');
+
+console.log(hash);
+
+
+//
+
+function  getHash(str) {
+    let hash = crypto.createHash('sha256').update(str).digest('hex');
+    return hash
+}
+
+for(let i=111110;i<=1000000;i++){
+    let h=getHash('100xdevs'+i.toString())
+    console.log(h)
+    if(h.startsWith('00')){
+        console.log(h)
+        break
+    }
+}
