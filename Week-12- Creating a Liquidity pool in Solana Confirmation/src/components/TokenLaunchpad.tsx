419252d3ee96bcc98a991f3c4765a50b8ff4dd05
= <=>
2024-10-26 19:24:27 +0530

    minting token

--- Week-12- Creating a Liquidity pool in Solana Confirmation/src/components/TokenLaunchpad.tsx
+++ Week-12- Creating a Liquidity pool in Solana Confirmation/src/components/TokenLaunchpad.tsx
@@ -7,16 +7,19 @@ import {
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 
-export default function TokenLaunchPad() {
+export default function TokenLaunchPad({
+  onTokenCreate,
+}: {
+  onTokenCreate: any;
+}) {
   const { connection } = useConnection();
-  console.log(connection);
 
   const wallet = useWallet();
 
   async function LauncToken() {
     const mintKeyPair = Keypair.generate();
     const lamports = await getMinimumBalanceForRentExemptMint(connection);
-
+    console.log("lamports", lamports, wallet.publicKey);
     if (wallet.publicKey) {
       const txn = new Transaction().add(
         SystemProgram.createAccount({
@@ -27,8 +30,57 @@ export default function TokenLaunchPad() {
           programId: TOKEN_PROGRAM_ID,
         })
       );
+
+      txn.feePayer = wallet.publicKey;
+      txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+      txn.partialSign(mintKeyPair);
+
+      await wallet.sendTransaction(txn, connection);
+      console.log(`Token mint created at ${mintKeyPair.publicKey.toBase58()}`);
+      onTokenCreate(mintKeyPair.publicKey);
     }
   }
 
-  return <></>;
+  return (
+    <>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Solana Token Launchpad</h1>
+        <input
+          className="inputText"
+          type="text"
+          placeholder="Name"
+        ></input>{" "}
+        <br />
+        <input
+          className="inputText"
+          type="text"
+          placeholder="Symbol"
+        ></input>{" "}
+        <br />
+        <input
+          className="inputText"
+          type="text"
+          placeholder="Image URL"
+        ></input>{" "}
+        <br />
+        <input
+          className="inputText"
+          type="text"
+          placeholder="Initial Supply"
+        ></input>{" "}
+        <br />
+        <button onClick={LauncToken} className="btn">
+          Create a token
+        </button>
+      </div>
+    </>
+  );
 }
