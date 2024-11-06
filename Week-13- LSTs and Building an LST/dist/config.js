02e59331f11f98d162295c3931f4f85c7bbb74d8
= <=>
2024-11-06 14:48:09 +0530

    minting token (incomplete)

---
+++ Week-13- LSTs and Building an LST/dist/config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.public_key = exports.private_key = exports.token_mint_address = void 0;
+exports.token_mint_address = "3mjVReHhUvrdoZuu7XkxoCjhEgSvotrXRa5WQZs7PRBm";
+exports.private_key = "";
+exports.public_key = "";
