a5d1acd4a50722d8b0b72d8754c09fa2ac45f9da
Dipak Khade <dipakhade214@gmail.com>
2025-02-08 15:50:26 +0530

    rustup

--- Week-26-Rust-Part-1/src/main.rs
+++ Week-26-Rust-Part-1/src/main.rs
@@ -1,3 +1,4 @@
+#[warn(unused_variables)]
 fn main() {
     let ans = sum(2,3);
     println!("{}",ans);
@@ -9,12 +10,25 @@ fn main() {
     println!("{:?}",arr);
 
     let str = String::from("dipak");
-    let len = get_len(str);
-    println!("{}",str)   // the ownership is moved of str to a variable s 
+    let len = get_len(&str);
+    println!("{}",str);   // the ownership is moved of str to a variable s 
+
+    // for i in 0..101{
+    //     println!("{}",i)
+    // }
+
+    let new_arr :[u32;4]= [4,5,6,8];
+    // let new_arr_iter:[u32;4] = new_arr.iter().map(|i| *i+1).collect();
+    for i in new_arr{
+        print!("value {}\n",i)
+    };
+
+    println!("{:?}",new_arr)
+
 
 }
 
-fn get_len(s:String)->usize{
+fn get_len(s:&str)->usize{
     s.len()
 }
 
@@ -25,3 +39,7 @@ fn sum(a:u32,b:u32)->u32{
 fn isEven(n:u32)->bool{
     n%2 ==0
 }
+
+// browwing and refereces
+
+
